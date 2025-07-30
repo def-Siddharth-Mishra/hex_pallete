4e7410128d4dc6a5ecb53d770750224f271b2766
Siddharth Mishra <siddharthmishra918@gmail.com>
2025-07-30 22:07:51 +0530

    Initial Commit

---
+++ hex_pallete/hex_palleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  hex_palleteApp.swift
+//  hex_pallete
+//
+//  Created by Siddharth Mishra on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct hex_palleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
